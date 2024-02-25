16870148a96c020095470db5082bd8e827e5afd1
ULTRAOS1 <161257198+ULTRAOS1@users.noreply.github.com>
2024-02-25 23:40:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['+212 688-528932']
+global.premium = ['+212 688-528932']
+global.ownernomer = '+212 688-528932'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
